0f9ce5c6344904d4dd2df17476e0d6404b1a9fc3
Amarico <Amarico@equipo-zalman>
2018-09-09 21:50:18 +0200

    Cambio enunciado del ejercicio

--- src/multiplodecincoconwhile/MultiploDeCincoConWhile.java
+++ src/multiplodecincoconwhile/MultiploDeCincoConWhile.java
@@ -12,10 +12,14 @@ package multiplodecincoconwhile;
 public class MultiploDeCincoConWhile {
 
   /**
+   * Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle while
+   * 
    * @param args the command line arguments
    */
   public static void main(String[] args) {
     // TODO code application logic here
+    
+    
   }
   
 }
